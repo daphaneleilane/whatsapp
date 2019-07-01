67c098671ded392a8154f89665a4664b95819306
William Gabriel <gabriel.ws@live.com>
2019-07-01 10:31:54 -0300

    Modificacao classe usuario

--- src/main/java/edu/br/whatsapp/modelo/Usuario.java
+++ src/main/java/edu/br/whatsapp/modelo/Usuario.java
@@ -1,11 +1,31 @@
 package edu.br.whatsapp.modelo;
 
+import java.util.List;
+
 public class Usuario {
 	private String nome;
 	private String foto;
 	private String recado;
 	private String numero;
+	private List<Usuario>contatos;
 
+
+	public Usuario(String nome, String foto, String recado, String numero, List<Usuario> contatos) {
+		super();
+		this.nome = nome;
+		this.foto = foto;
+		this.recado = recado;
+		this.numero = numero;
+		this.contatos = contatos;
+	}
+
+	public List<Usuario> getContatos() {
+		return contatos;
+	}
+
+	public void setContatos(List<Usuario> contatos) {
+		this.contatos = contatos;
+	}
 
 	public Usuario(String nome, String numero) {
 		super();
